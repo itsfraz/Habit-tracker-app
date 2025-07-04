d472ef545a07d5fdec5c7de41ecd928a1354d324
Faraz4807 <farajm624@mail.com>
2025-07-04 11:30:39 +0530

    functional server  commit

--- habit-tracker/src/services/habitService.js
+++ habit-tracker/src/services/habitService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authService from './authService';
 
-const API_URL = 'https://habit-tracker-app-lwym.onrender.com:5000/api/habits/';
+const API_URL = 'http://localhost:5000/api/habits/';
 
 const getAuthHeader = () => {
   const user = authService.getCurrentUser();
