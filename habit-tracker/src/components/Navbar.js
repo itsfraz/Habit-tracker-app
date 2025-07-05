5349b4441655b484e372e7d6132d1b9846a71d26
Mohd Faraj Ansari <109614407+itsfraz@users.noreply.github.com>
2025-07-05 16:06:18 +0530

    Update Navbar.js

--- habit-tracker/src/components/Navbar.js
+++ habit-tracker/src/components/Navbar.js
@@ -1,7 +1,5 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
-import authService from '../services/authService';
 
 const Navbar = ({ currentUser, logOut }) => {
   return (
